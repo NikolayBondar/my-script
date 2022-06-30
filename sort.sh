d59832db1647dc61e9151ae34777f09721eb6f3d
Nikolay Bondar <nikolai.bondar@gmail.com>
2022-06-30 19:17:13 +0300

    add func

--- sort.sh
+++ sort.sh
@@ -6,3 +6,7 @@ find /var/log/ -maxdepth 1 -type f -exec ls -la {} \; | sort -nk5 | cut -d " " -
 
 # Includes subdir
 find /var/log -type f -exec ls -la {} \; | sort -nk5 | cut -d " " -f5,9 > ./logR.txt
+
+# В некоторых случаях предыдущий вариант некорректно отрабатывал по причине того что разделитель колонок (пробел), 
+# в некоторых строках оказывался двойным, функция некорректно разбивала на колонки, вышел из ситуации так:
+find /var/log -maxdepth 1 -type f -exec ls -l {} \+ | sort -nk5 | cut -b 47- >log.txt
