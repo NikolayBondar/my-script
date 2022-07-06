d1e12bdc110bfaf16b8f6f949f49829acc37e54d
Nikolay Bondar <nikolai.bondar@gmail.com>
2022-07-06 15:58:34 +0300

    Cange

--- sort.sh
+++ sort.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
 # Only directory "log"
-find /var/log/ -maxdepth 1 -type f -exec ls -la {} \; | sort -nk5 | cut -d " " -f5,9 > ./log.txt
+find /var/log -maxdepth 1 -type f -exec ls -l {} \+ | sort -nk5 | tr -s " " | cut -d " " -f9- > log.txt
 
 # Includes subdir
-find /var/log -type f -exec ls -la {} \; | sort -nk5 | cut -d " " -f5,9 > ./logR.txt
+find /var/log -type f -exec ls -l {} \+ | sort -nk5 | tr -s " " | cut -d " " -f9- > logR.txt
 
-# В некоторых случаях предыдущий вариант некорректно отрабатывал по причине того что разделитель колонок (пробел), 
-# в некоторых строках оказывался двойным, функция некорректно разбивала на колонки, вышел из ситуации так:
-find /var/log -maxdepth 1 -type f -exec ls -l {} \+ | sort -nk5 | cut -b 47- >log.txt
