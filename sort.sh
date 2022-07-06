eabc3c9334770419d1456752c75f1b1af0a6ab66
Nikolay Bondar <nikolai.bondar@gmail.com>
2022-07-06 16:02:03 +0300

    Edit

--- sort.sh
+++ sort.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Only directory "log"
-find /var/log -maxdepth 1 -type f -exec ls -l {} \+ | sort -nk5 | tr -s " " | cut -d " " -f9- > log.txt
+find /var/log -maxdepth 1 -type f -exec ls -l {} \+ | sort -nk5 | tr -s " " | cut -d " " -f9 > log.txt
 
 # Includes subdir
-find /var/log -type f -exec ls -l {} \+ | sort -nk5 | tr -s " " | cut -d " " -f9- > logR.txt
+find /var/log -type f -exec ls -l {} \+ | sort -nk5 | tr -s " " | cut -d " " -f9 > logR.txt
 
